11b84eb07014a0ba105f2fd70047732489090690
haris <anas.ejaz1991@gmail.com>
2018-01-02 19:48:52 +0500

    import jsqviewcontroller and dispaly bubbles

--- ChatUISwiftTwo/ViewController.swift
+++ ChatUISwiftTwo/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import JSQMessagesViewController
 
-class ViewController: UIViewController {
+struct User{
+    
+    let id:String
+    let name:String
+}
+
+
+class ViewController:JSQMessagesViewController {
+    
+    
+    let userone = User(id: "1", name: "Haris")
+    let userTwo = User(id: "2", name: "Rehan")
+    
+    var currentUser:User{
+        
+        return userone
+        
+    }
+    var messages = [JSQMessage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.senderId = self.currentUser.id
+        self.senderDisplayName = self.currentUser.name
+        
+        
+        print(self.senderId)
+        print(self.senderDisplayName)
+        self.messages = self.getMessages()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func getMessages()->[JSQMessage]{
+        
+        
+        var messagesArray = [JSQMessage]()
+        
+        let messageOne = JSQMessage(senderId: "1", displayName: "Haris", text: "hi rehan how are U?")
+        let messageTwo = JSQMessage(senderId: "2", displayName: "Rehan", text: "I am Fine And U?")
+        
+        messagesArray.append(messageOne!)
+        messagesArray.append(messageTwo!)
+        
+        return messagesArray
+        
     }
 
+    
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return self.messages.count
+
+        
+    }
+    
+
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+         return self.messages[indexPath.row]
+    }
+    
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        
+        let bubbleFactory = JSQMessagesBubbleImageFactory()
+        
+     
+        
+        return bubbleFactory.incomingMessagesBubbleImageWithColor(UIColor.greenColor())
+        
+    }
+    
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
 
 }
 
